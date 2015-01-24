467dbe52208dd14589931a8344dce9b7ea077c65
Taiiwo Llort <tylerzacc@gmail.com>
2015-01-24 20:34:32 +0000

    remove freewall

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,11 @@
 function Deck(id){
 	this.id = id;
-	this.table = new freewall(id);
-	this.table.reset({
-		selector: '.card',
-		animate: false,
-		cellW: 200,
-		cellH: 'auto',
-		onResize: function() {
-			this.table.fitWidth();
-		}
-	});
+	this.table = $(this.id);
 	this.cards = [];
-	this.update = function() {
-		this.table.refresh();
-	}
-	this.update();
 	this.addCard = function(title, desc, img){
 		//create card
 		var card = '\
-		<routine-card class="card">\
+		<routine-card>\
 			<h1>'+ title +'</h1>\
 			<li>'+ desc +'</li>\
 			<img src="'+ img +'">\
@@ -26,12 +13,10 @@ function Deck(id){
 		// add card to list of cards
 		this.cards.push(card);
 		// add a card to the deck
-		this.table.appendBlock(card);
+		this.table.append(card);
 	}
 }
-var routines = new Deck(".routines");
-routines.update();
-
+var routines = new Deck("routine-list");
 routines.addCard("test","test","http://i.imgur.com/IUIVk80.jpg");
 routines.addCard("test","test","http://i.imgur.com/IUIVk80.jpg");
 routines.addCard("test","test","http://i.imgur.com/IUIVk80.jpg");
