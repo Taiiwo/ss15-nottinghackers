2672caabab8208b0150a8461c89c555a4ed86ff7
Taiiwo Llort <tylerzacc@gmail.com>
2015-01-24 15:35:38 +0000

    use James' HTML for the cards

--- scripts/main.js
+++ scripts/main.js
@@ -20,25 +20,12 @@ function Deck(id){
 	this.update();
 	this.add = function(title, desc, img){
 		//create card
-		var card = $('<div>')
-			.addClass('card')
-			.append( $('<img>')
-				.attr("src", img) 
-				.attr("width", "100%")
-			)
-			.append( $('<div>')
-				.addClass("cardInfo")
-				.append(
-					$('<h3>')
-						.addClass("cardTitle")
-						.text(title)
-				)
-				.append(
-					$('<h5>')
-						.addClass("cardDesc")
-						.text(desc)
-				)
-			);
+		var card = '\
+		<routine-card>\
+			<h1>'+ title +'</h1>\
+			<ul>'+ desc +'</ul>\
+			<img src="'+ img +'">\
+		</routine-card>';
 		// add card to list of cards
 		this.cardList.push(card);
 		// add a card to the deck
