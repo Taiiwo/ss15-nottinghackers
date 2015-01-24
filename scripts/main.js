b46b4d4577eecf229405337123cb7653d854fd13
James Brooks <james.brooks2@btinternet.com>
2015-01-24 23:45:21 +0000

    Added WebRTC video in a temporary place for now

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,20 @@
 $('#addButton').click(function() {
-	var dialog = document.querySelector('html /deep/ paper-dialog');
+	var dialog = document.querySelector('html /deep/ #addRoutine');
 	dialog.toggle();
 });
+
+navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+
+var errorCallback = function(e) {
+	console.log('Rejected!', e);
+};
+
+var video = document.querySelector('#mirror');
+if (navigator.getUserMedia) {
+  	navigator.getUserMedia({
+  		audio: false, video: true
+  	}, function(stream) {
+   		video.src = window.URL.createObjectURL(stream);
+   		video.play();
+  	}, errorCallback);
+};
