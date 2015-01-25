81b24dcf52984d37720dc35c67bcef19a36b443f
Taiiwo Llort <tylerzacc@gmail.com>
2015-01-25 12:46:12 +0000

    One can now save routines to the database

--- scripts/main.js
+++ scripts/main.js
@@ -35,7 +35,7 @@ var addRoutine = function(){
 		// put a canvas inside the box
 		this.container.append('<canvas id="poseInput" width="500" height="500"></canvas>');
 		// make a canvas with a stick man inside
-		this.canvasObj = function(id){// this is a class that makes a stick man input
+		this.canvasObj = function(id,head, body, armL, armL2, armR, armR2, legL, legL2, legR, legR2){// this is a class that makes a stick man
 			this.canvas = this.__canvas = new fabric.Canvas(id, { selection: false });
 			var thisSubThis = this;
 			fabric.Object.prototype.originX = fabric.Object.prototype.originY = 'center';
@@ -57,7 +57,6 @@ var addRoutine = function(){
 
 				return c;
 			}
-
 			this.makeLine = function(coords) {
 				return new fabric.Line(coords, {
 					fill: 'white',
@@ -67,39 +66,38 @@ var addRoutine = function(){
 				});
 			}
 			
-			this.head = this.makeLine([ 250, 125, 250, 175 ]);
-			this.body = this.makeLine([ 250, 175, 250, 250 ]);
-			this.armL = this.makeLine([ 250, 175, 175, 200 ]);
-			this.armL2 = this.makeLine([ 175, 200, 175, 250 ]);
-			this.armR = this.makeLine([ 250, 175, 325, 200 ]);
-			this.armR2 = this.makeLine([ 325, 200, 325, 250 ]);
-			this.legL = this.makeLine([ 250, 250, 200, 300]);
-			this.legL2 = this.makeLine([ 200, 300, 200, 375]);
-			this.legR = this.makeLine([ 250, 250, 300, 300]);
-			this.legR2 = this.makeLine([ 300, 300, 300, 375]);
-				
-
-			this.canvas.add(this.head, this.body, this.armL, this.armL2, this.armR, this.armR2, this.legL, this.legL2, this.legR, this.legR2);
+			head = this.makeLine(head);
+			body = this.makeLine(body);
+			armL = this.makeLine(armL);
+			armL2 = this.makeLine(armL2);
+			armR = this.makeLine(armR);
+			armR2 = this.makeLine(armR2);
+			legL = this.makeLine(legL);
+			legL2 = this.makeLine(legL2);
+			legR = this.makeLine(legR);
+			legR2 = this.makeLine(legR2);
 			
-			var headCircle = this.makeCircle(this.head.get('x1'), this.head.get('y1'), null, this.head);
+				
+			this.canvas.add(head, body, armL, armL2, armR, armR2, legL, legL2, legR, legR2);
+			
+			var headCircle = this.makeCircle(head.get('x1'), head.get('y1'), null, head);
 			headCircle.radius = 30;
 			headCircle.fill = 'white';
 			
 			this.canvas.add(
 				headCircle,
-				this.makeCircle(this.head.get('x2'), this.head.get('y2'), this.head, this.body, this.armL, this.armR),
-				this.makeCircle(this.body.get('x2'), this.body.get('y2'), this.body, this.legR, this.legL),
-				this.makeCircle(this.legR.get('x2'), this.legR.get('y2'), this.legR, this.legR2),
-				this.makeCircle(this.legR2.get('x2'), this.legR2.get('y2'), this.legR2),
-				this.makeCircle(this.legL.get('x2'), this.legL.get('y2'), this.legL, this.legL2),
-				this.makeCircle(this.legL2.get('x2'), this.legL2.get('y2'), this.legL2),
-				this.makeCircle(this.armL.get('x2'), this.armL.get('y2'), this.armL, this.armL2),
-				this.makeCircle(this.armL2.get('x2'), this.armL2.get('y2'), this.armL2),
-				this.makeCircle(this.armR.get('x2'), this.armR.get('y2'), this.armR, this.armR2),
-				this.makeCircle(this.armR2.get('x2'), this.armR2.get('y2'), this.armR2)
+				this.makeCircle(head.get('x2'), head.get('y2'), head, body, armL, armR),
+				this.makeCircle(body.get('x2'), body.get('y2'), body, legR, legL),
+				this.makeCircle(legR.get('x2'), legR.get('y2'), legR, legR2),
+				this.makeCircle(legR2.get('x2'), legR2.get('y2'), legR2),
+				this.makeCircle(legL.get('x2'), legL.get('y2'), legL, legL2),
+				this.makeCircle(legL2.get('x2'), legL2.get('y2'), legL2),
+				this.makeCircle(armL.get('x2'), armL.get('y2'), armL, armL2),
+				this.makeCircle(armL2.get('x2'), armL2.get('y2'), armL2),
+				this.makeCircle(armR.get('x2'), armR.get('y2'), armR, armR2),
+				this.makeCircle(armR2.get('x2'), armR2.get('y2'), armR2)
 			);
 			
-
 			this.canvas.on('object:moving', function(e) {
 				var p = e.target;
 				p.line1 && p.line1.set({ 'x2': p.left, 'y2': p.top });
@@ -109,73 +107,84 @@ var addRoutine = function(){
 				thisSubThis.canvas.renderAll();
 			});
 			this.dumpCoords = function(){
-				console.log({
+				return {
 					"head":[
-						this.head.get('x1'),
-						this.head.get('y1'),
-						this.head.get('x2'),
-						this.head.get('y2')
+						head.get('x1'),
+						head.get('y1'),
+						head.get('x2'),
+						head.get('y2')
 					],
 					"body":[
-						this.body.get('x1'),
-						this.body.get('y1'),
-						this.body.get('x2'),
-						this.body.get('y2')
+						body.get('x1'),
+						body.get('y1'),
+						body.get('x2'),
+						body.get('y2')
 					],
 					"armL":[
-						this.armL.get('x1'),
-						this.armL.get('y1'),
-						this.armL.get('x2'),
-						this.armL.get('y2')
+						armL.get('x1'),
+						armL.get('y1'),
+						armL.get('x2'),
+						armL.get('y2')
 					],
 					"armL2":[
-						this.armL2.get('x1'),
-						this.armL2.get('y1'),
-						this.armL2.get('x2'),
-						this.armL2.get('y2')
+						armL2.get('x1'),
+						armL2.get('y1'),
+						armL2.get('x2'),
+						armL2.get('y2')
 					],
 					"armR":[
-						this.armR.get('x1'),
-						this.armR.get('y1'),
-						this.armR.get('x2'),
-						this.armR.get('y2')
+						armR.get('x1'),
+						armR.get('y1'),
+						armR.get('x2'),
+						armR.get('y2')
 					],
 					"armR2":[
-						this.armR2.get('x1'),
-						this.armR2.get('y1'),
-						this.armR2.get('x2'),
-						this.armR2.get('y2')
+						armR2.get('x1'),
+						armR2.get('y1'),
+						armR2.get('x2'),
+						armR2.get('y2')
 					],
 					"legL":[
-						this.legL.get('x1'),
-						this.legL.get('y1'),
-						this.legL.get('x2'),
-						this.legL.get('y2')
+						legL.get('x1'),
+						legL.get('y1'),
+						legL.get('x2'),
+						legL.get('y2')
 					],
 					"legL2":[
-						this.legL2.get('x1'),
-						this.legL2.get('y1'),
-						this.legL2.get('x2'),
-						this.legL2.get('y2')
+						legL2.get('x1'),
+						legL2.get('y1'),
+						legL2.get('x2'),
+						legL2.get('y2')
 					],
 					"legR":[
-						this.legR.get('x1'),
-						this.legR.get('y1'),
-						this.legR.get('x2'),
-						this.legR.get('y2')
+						legR.get('x1'),
+						legR.get('y1'),
+						legR.get('x2'),
+						legR.get('y2')
 					],
 					"legR2":[
-						this.legR2.get('x1'),
-						this.legR2.get('y1'),
-						this.legR2.get('x2'),
-						this.legR2.get('y2')
+						legR2.get('x1'),
+						legR2.get('y1'),
+						legR2.get('x2'),
+						legR2.get('y2')
 					]
-				});
+				};
 			}
 			
 		}
 		// init the input
-		this.poseInput = new this.canvasObj("poseInput");
+		this.poseInput = new this.canvasObj("poseInput",
+			[ 250, 125, 250, 175 ],
+			[ 250, 175, 250, 250 ],
+			[ 250, 175, 175, 200 ],
+			[ 175, 200, 175, 250 ],
+			[ 250, 175, 325, 200 ],
+			[ 325, 200, 325, 250 ],
+			[ 250, 250, 200, 300 ],
+			[ 200, 300, 200, 375 ],
+			[ 250, 250, 300, 300 ],
+			[ 300, 300, 300, 375 ]
+		);
 	}
 	this.closeDialog = function(){
 		// hide the dialog
@@ -192,6 +201,39 @@ var addRoutine = function(){
 		else {
 			subThis.openDialog();
 		}
+	}
+	this.routine = [];
+	this.addRoutineElement = function(title, desc, reps, duration){
+		this.routine.push({
+			"title":title,
+			"desc":desc,
+			"reps":reps,
+			"duration":duration,
+			"pose": this.poseInput.dumpCoords()
+		});
+		this.updateRoutine();
+	}
+	this.updateRoutine = function(){
+		// fill up the routine list with the routine elements
+		// something like
+		/*
+		this.routine.each(function(index){
+			$('routine-element-list')
+				.append(
+					$("<h3>").text(this.title)
+				)
+				.append(
+					$("h5").text(this.reps)
+				);
+		});
+		*/
+	}
+	this.uploadRoutine = function(title){
+		    var DB = new Firebase("https://ss15.firebaseio.com/");
+			DB.push({
+				"title": title,
+				"routine": this.routine
+			});
 	}
 }
 // init the add routine button
